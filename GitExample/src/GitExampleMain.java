20fd085d95f13c36d7a7b0a5e8154871f666930b
Rachael <Rachael@Rachael-PC>
2015-12-26 14:37:03 -0500

    Added Scanner

--- GitExample/src/GitExampleMain.java
+++ GitExample/src/GitExampleMain.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 
 public class GitExampleMain {
+	Scanner scan = new Scanner(System.in);
 
 	public static void main(String args[]){
 		System.out.println("This is a git Example");
