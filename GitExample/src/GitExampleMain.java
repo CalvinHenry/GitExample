60680d1ac071fc0804fe62ccf8357550b60acbb8
Rachael <Rachael@Rachael-PC>
2015-12-26 14:35:25 -0500

    Added A print method

--- GitExample/src/GitExampleMain.java
+++ GitExample/src/GitExampleMain.java
@@ -4,4 +4,7 @@ public class GitExampleMain {
 	public static void main(String args[]){
 		System.out.println("This is a git Example");
 	}
+	public static void print(String s){
+		System.out.println(s);
+	}
 }
